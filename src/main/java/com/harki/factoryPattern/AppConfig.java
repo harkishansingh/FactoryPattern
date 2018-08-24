64f609172e5642f4ed6104c804b6caa546261222
Harkishan Singh <harkishansingh.1989@gmail.com>
2018-08-24 16:48:25 +0530

    Update AppConfig.java

--- src/main/java/com/harki/factoryPattern/AppConfig.java
+++ src/main/java/com/harki/factoryPattern/AppConfig.java
@@ -7,4 +7,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages="com.harki.factoryPattern")
 public class AppConfig{
 	
+	public static void main(String[] args){
+		System.out.println("Hello World!!");
+	}
+	
 }
